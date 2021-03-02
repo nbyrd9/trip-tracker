9b8f1df725d655ecf7100cfc5d2c25185dd09faf
nbyrd9 <nianibyrd@gmail.com>
2021-03-02 13:04:13 -0500

    updates Users Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,15 @@ class ApplicationController < Sinatra::Base
     get '/' do
         erb :home
     end
+
+    helpers do 
+        def logged_in?
+            !!current_user
+        end
+
+        def current_user
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
 end
 
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+
+    get '/signup' do
+        if logged_in?
+            @user = current_user
+            erb :'users/login' ##VIEWS
+        end
+        erb :'users/signup'
+    end
+
+
+    post '/signup' do
+        @user = User.create(params[:user])
+        if @user.id 
+            session[:user_id] = @user.id
+            redirect '/users/#{@user.id}'
+        else
+            erb :'users/error'
+    end
+
+    get '/users/:id' do
+        redirect_if_not_logged_in
+            @user = User.find_by(id: params[:id])
+            @trips = @user.trips
+            erb :'users/show'
+    end
+
+    get '/login' do 
+        if !logged_in?
+            erb :'users/login' ##VIEWS
+        else
+            redirect "/users/#{session[:user_id]}"
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:user][:username])
+
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            flash[:message] = "You have successfully logged in!"
+            redirect "/users/#{user.id}"
+        else
+            @errors = ["Invalid login. Please try again."]
+            erb :'users/login'
+        end
+    end
+
+    get '/logout' do 
+        session.destroy
+        redirect '/login'
+    end
+
