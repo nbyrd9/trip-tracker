45f01991b6e44f96919550ec71d620a82f933d6f
nbyrd9 <nianibyrd@gmail.com>
2021-03-03 08:08:36 -0500

    allows user to create new trip after validating login

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,7 +6,30 @@ class TripsController < ApplicationController
         erb :'items/index'
     end
 
+    get '/trips/new' do 
+        if logged_in?
+            @user = current_user
+            erb :'/trips/new'
+        else
+            redirect '/home'
+        end
+    end
+
+    post '/trips' do
+        redirect_if_not_logged_in
+        trip = Trip.create(params[:trip])
+        user = User.find_by(id: session[:user_id])
+        user.trips << trip
+    end
 
     
+
+
+
+
+
+
+
+
 
 end
